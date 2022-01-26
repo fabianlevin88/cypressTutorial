666687a68720d74f428ab47c243b08c0e220fab9
Fabian Andres Levin <fabianlevin88@hotmail.com>
2022-01-26 11:42:54 -0300

    Added a headless mode run script and disabled video and screenshots when failling a test

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -3,7 +3,9 @@ describe('My first simple test with cypress', () => {
     it('Example of passing test', () => {
         expect(true).to.equal(true)
     })
+})
 
+describe('Failling test example', () => {
     it('Example of failling test', () => {
         expect(true).to.equal(false)
     })
