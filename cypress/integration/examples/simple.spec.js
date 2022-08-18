ce393fa92e139954e1af677e8e8ed232e5121f33
Fabian Andres Levin <fabianlevin88@hotmail.com>
2022-08-18 14:54:59 -0300

    added the command cy:run to run a testing suite in headless mode

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -1,12 +1,6 @@
 describe('My first simple test with cypress', () => {
-    
     it('Example of passing test', () => {
         expect(true).to.equal(true)
-    })
-})
-
-describe('Failling test example', () => {
-    it('Example of failling test', () => {
-        expect(true).to.equal(false)
+        console.log('passing test')
     })
 })
